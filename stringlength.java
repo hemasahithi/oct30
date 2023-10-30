0e2aa6f98d1606c63519b6c962f56b78e2bfaa28
sahithi <admin@gmail.com>
2023-10-30 17:50:31 +0530

    commiting the file

---
+++ stringlength.java
@@ -0,0 +1,13 @@
+package project3;
+
+public class stringlength {
+
+	public static void main(String[] args) {
+		String text = "Hi, My name is Nagasahithi";
+  int Length = text.length();
+ System.out.println("Length of the string is : " + Length);
+ 
+ 
+	}
+
+}
